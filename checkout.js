d07f391c30b10e9410dc7dbb5872a6bb51478dca
toNReverse <mark04sap@gmail.com>
2025-07-10 19:17:55 +0200

    Definitivo

--- checkout.js
+++ checkout.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((res) => res.json())
         .then((data) => {
           if (data.id) {
-            const stripe = Stripe(""); // CHIAVE PUBBLICA
+            const stripe = Stripe("pk_test_51RiHdmRspCzH4l9NL0Co7Y58T3jVKQoOEnLn2CbrLhbdKlMvD3uPgHKWg7AYdZpKVVY2JQJebcW40q9WcRh2wUYu00w9zO2lmh"); // CHIAVE PUBBLICA
             stripe.redirectToCheckout({ sessionId: data.id });
           } else {
             console.error("Errore lato server:", data.error || "Session ID mancante");
--- wish-list.php
+++ wish-list.php
@@ -15,6 +15,8 @@
   <script src="cart-modal.js" defer></script>
   <script src="https://js.stripe.com/v3/"></script>
   <script src="checkout.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
 </head>
 <body>
